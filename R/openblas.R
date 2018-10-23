582aecc6c9700c42efef43916344e5efb5b8f183
Colin Rundel <rundel@gmail.com>
2018-10-22 23:34:14 -0400

    Add get_num_threads and clean up interface

--- R/openblas.R
+++ R/openblas.R
@@ -1,9 +1,49 @@
-set_num_threads = function(threads) {
+#' Wrapper to set the number of OpenBlas threads from within an R session, via 
+#' openblas_set_num_threads(), for implicit parallelization. Alters the number 
+#' of threads employed by OpenBlas for that session and child sessions. 
+#'
+#' @param threads Maximum number of threads to use for OpenBlas operations
+#'
+#' @examples
+#' mat = matrix(seq_len(5e7),5e4,1e3)
+#' 
+#' set_num_threads(1)
+#' system.time( crossprod(mat,mat) )
+#' 
+#' set_num_threads(2)
+#' system.time( crossprod(mat,mat) )
+#'
+#' set_num_threads(4)
+#' system.time( crossprod(mat,mat) )
+#' 
+#' @useDynLib OpenBlasThreads obt_set_num_threads
+#' 
+#' @export
+
+set_num_threads = function(threads = parallel::detectCores()) {
   threads = as.integer(threads)
   
   stopifnot(length(threads) == 1)
   stopifnot(!is.na(threads))
-  stopifnot(theads >= 1 &threads <= parallel::detectCores())
+  stopifnot(threads >= 1 &threads <= parallel::detectCores())
   
-	.C("set_blas", ipt=threads, PACKAGE="OpenBlasThreads")
+  invisible(
+	  .Call("obt_set_num_threads", n=threads, PACKAGE="OpenBlasThreads")
+  )
+}
+
+#' Get the number of allowed OpenBlas threads for current R session.
+#'
+#' @examples
+#' get_num_threads()
+#' set_num_threads(2)
+#' get_num_threads()
+#' 
+#' @useDynLib OpenBlasThreads obt_get_num_threads
+#' 
+#' @export
+
+get_num_threads = function() {
+  threads = NA_integer_
+  .Call("obt_get_num_threads", PACKAGE="OpenBlasThreads")
 }
--- src/set_blas.c
+++ src/set_blas.c
@@ -1,5 +1,18 @@
-extern void openblas_set_num_thread(int);	//may not be needed
+#include <R.h>
+#include <Rinternals.h>
 
-void set_blas( int* ipt ) {
-	openblas_set_num_threads( *ipt );
+extern void openblas_set_num_thread(int);	
+extern int  openblas_get_num_thread(void);
+
+SEXP obt_set_num_threads(SEXP n) {
+	openblas_set_num_threads(INTEGER(n));
+  return R_NilValue;
+}
+
+SEXP obt_get_num_threads() {
+  SEXP n = PROTECT(allocVector(INTSXP, 1));
+  INTEGER(n)[0] = openblas_get_num_threads();
+  UNPROTECT(1);
+  
+  return n;
 }
